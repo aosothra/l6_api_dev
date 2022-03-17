4d8219a6397bd9138952b8d0f1d3da2c834e86e6
aosothra <arhdam@yandex.ru>
2022-03-17 15:32:59 +0400

    add relevant api method wrapping functions

--- vk_basic.py
+++ vk_basic.py
@@ -1,12 +1,46 @@
 import requests
 
 
+def get_wall_upload_server(vk_token, vk_group):
+    params = {
+        'group_id': vk_group
+    }
+    result = call_vk_api('photos.getWallUploadServer', vk_token, params)
+    return result['upload_url']
+
+
+def save_wall_photo(vk_token, vk_group, photo_properties):
+    params = {
+        'group_id': vk_group,
+        'photo': photo_properties['photo'],
+        'server': photo_properties['server'],
+        'hash': photo_properties['hash']
+    }
+    result = call_vk_api('photos.saveWallPhoto', vk_token, params)[0]
+    print(result)
+    return result['id'], result['owner_id']
+
+
+def wall_post(vk_token, vk_group, photo_id, owner_id, text):
+    params = {
+        'owner_id': -vk_group,
+        'from_group': 1,
+        'message': text,
+        'attachments': [f"photo{owner_id}_{photo_id}"]
+    }
+    result = call_vk_api('wall.post', vk_token, params)
+    return result
+
+
 def call_vk_api(method, access_token, params):
     url = f'https://api.vk.com/method/{method}'
-    params['access_token'] = access_token
-    params['v'] = '5.81'
+    payload = {
+        'access_token': access_token,
+        'v': '5.81',
+        **params
+    }
 
-    response = requests.get(url, params=params)
+    response = requests.get(url, params=payload)
     response.raise_for_status()
 
     data = response.json()
